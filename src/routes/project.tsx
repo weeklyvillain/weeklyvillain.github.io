38b3b52a2c0f300fa58b8aa72f52f2a83e1aa110
Filip Eriksson <fmeriksson98@gmail.com>
2024-05-15 17:56:46 +0200

    fixing eslint

--- src/routes/project.tsx
+++ src/routes/project.tsx
@@ -5,7 +5,7 @@ import ProjectList from '../projects/ProjectList.json'
 import { NavBar } from "../components/NavBar";
 import { Carousel } from "../components/Carousel";
 
-interface Project{
+interface ProjectData{
   id: string,
   title: string,
   url: string,
@@ -20,7 +20,7 @@ interface Project{
 export default function Project() {
     let { id } = useParams();
 
-    const projects: Project[] = ProjectList as Project[];
+    const projects: ProjectData[] = ProjectList as ProjectData[];
     const project = projects.find(project => project.id === id);
 
     document.title = project?.title || "Project";
@@ -30,8 +30,8 @@ export default function Project() {
         <NavBar></NavBar>
         <div className="flex pt-32">
           <div className="w-1/3 ml-10">
-            <h1 className={"text-5xl font-bold"}>{project?.title}</h1>
-            <p className={"py-6 max-w-2xl"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in blandit odio. Cras vestibulum convallis efficitur. Cras in lorem eu justo fringilla ultrices vel sit amet risus. Morbi et ex a nisi gravida iaculis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a nibh quis tortor gravida sagittis. Sed non eros at sapien pretium aliquet non ut elit. Sed ornare cursus convallis. Integer aliquam vel nisl et ultrices. Sed fringilla nisl et scelerisque lacinia.
+            <h1 className="text-5xl font-bold">{project?.title}</h1>
+            <p className="py-6 max-w-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in blandit odio. Cras vestibulum convallis efficitur. Cras in lorem eu justo fringilla ultrices vel sit amet risus. Morbi et ex a nisi gravida iaculis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a nibh quis tortor gravida sagittis. Sed non eros at sapien pretium aliquet non ut elit. Sed ornare cursus convallis. Integer aliquam vel nisl et ultrices. Sed fringilla nisl et scelerisque lacinia.
 
 Maecenas a lectus vitae elit luctus tristique eu vitae velit. Cras semper mollis enim, et ultricies est ultrices quis. Suspendisse potenti. Etiam arcu magna, convallis quis libero condimentum, dictum luctus nulla. Donec vehicula hendrerit sem ac ullamcorper. Vestibulum finibus erat eget sem rutrum commodo. Etiam sagittis lorem a lorem luctus laoreet. Fusce quis blandit augue. In commodo velit non turpis ultricies blandit. Sed mattis gravida ullamcorper. Proin at suscipit quam. Phasellus convallis elit a dictum convallis. Integer sodales sollicitudin dui eu sollicitudin. Suspendisse massa elit, pellentesque sed consectetur nec, rutrum nec eros. Curabitur scelerisque in nisl id efficitur.
 
@@ -41,10 +41,10 @@ Maecenas placerat euismod tortor id scelerisque. Suspendisse sem nisi, fermentum
           </div>
           <div className="w-2/3">
             {
-              project?.images.gallery.length == 0 && <img src={project?.images.hero} alt="" className="p-24 pt-0 w-full rounded-lg shadow-2xl" />
+              project?.images.gallery.length === 0 && <img src={project?.images.hero} alt="dasdasd" className="p-24 pt-0 w-full rounded-lg shadow-2xl" />
             }
             {
-              project?.images.gallery.length != 0 && <Carousel images={project?.images.gallery || []} />
+              project?.images.gallery.length !== 0 && <Carousel images={project?.images.gallery || []} />
             }
           </div>
         </div>
