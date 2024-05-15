f34cc58effe653225f3b0716d8e6fd8f4e9c277c
Filip Eriksson <fmeriksson98@gmail.com>
2024-05-15 17:58:46 +0200

    fixing eslint

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,10 +19,10 @@ export const Hero = (): JSX.Element => {
   return (
     <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="" className={showImage ? "opacity-100 translate-x-10" : "opacity-0 translate-x-32" + " max-w-sm rounded-lg shadow-2xl duration-1000	transition-all ease-out"} />
+            <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="" className={showImage ? "opacity-100 translate-x-10 max-w-sm rounded-lg shadow-2xl duration-1000	transition-all ease-out" : "opacity-0 translate-x-32 max-w-sm rounded-lg shadow-2xl duration-1000	transition-all ease-out"} />
             <div>
-              <h1 className={showText ? "opacity-100 translate-y-0" : "opacity-0 translate-y-24" + " text-5xl font-bold duration-700	transition-all ease-out"}>Welcome!</h1>
-              <p className={showText ? "opacity-100 translate-y-0" : "opacity-0 translate-y-24" + " py-6 duration-700 transition-all ease-out"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id justo sit amet dolor efficitur <br /> egestas vitae in duiasd sad asdasd asd asd </p>
+              <h1 className={showText ? "opacity-100 translate-y-0 text-5xl font-bold duration-700	transition-all ease-out" : "opacity-0 translate-y-24 text-5xl font-bold duration-700	transition-all ease-out"}>Welcome!</h1>
+              <p className={showText ? "opacity-100 translate-y-0 py-6 duration-700 transition-all ease-out" : "opacity-0 translate-y-24 py-6 duration-700 transition-all ease-out"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id justo sit amet dolor efficitur <br /> egestas vitae in duiasd sad asdasd asd asd </p>
             </div>
         </div>
     </div>
